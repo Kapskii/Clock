d2e4dd1e83d8d8841663cd2858f7a455ce3f4498
Kapskii <kapskij20@gmail.com>
2023-10-03 11:10:21 +0300

    fix bug

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+isComing = false;
 let timerId;
 
 
 const startButton = document.getElementById('start');
 startButton.addEventListener('click', function () {
+    if (!isComing) {
     timerId = setInterval(updateClock, 1000);
+    isComing = true;
+}
 });
 
 
 const stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', function () {
+    if (isComing) {
     clearInterval(timerId);
+    isComing = false;
+}
 });
+
 
 
 function updateClock() {
